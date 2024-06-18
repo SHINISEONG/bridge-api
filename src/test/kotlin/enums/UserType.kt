c59e4d4b5a0be4b0415e1bc0e0158367bc79e8f9
shineseong <hss275989@gmail.com>
2024-06-18 20:44:31 +0900

    fix: testEnum Buyer.value  변경.

--- src/test/kotlin/enums/UserType.kt
+++ src/test/kotlin/enums/UserType.kt
@@ -3,7 +3,7 @@ package enums
 enum class UserType(val value: Int) {
     ADMIN(0),
     SELLER(1),
-    BUYER(3),
+    BUYER(2),
     ;
     companion object {
         fun from(value: Int): UserType = entries.firstOrNull { it.value == value } ?: throw IllegalArgumentException()
