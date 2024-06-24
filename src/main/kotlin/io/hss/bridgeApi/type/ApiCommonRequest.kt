41696ade788a34da7bf1fe9423418a05e0969a04
shineseong <hss275989@gmail.com>
2024-06-24 17:31:19 +0900

    fix: change the property name of ApiCommonRequest

--- src/main/kotlin/io/hss/bridgeApi/type/ApiCommonRequest.kt
+++ src/main/kotlin/io/hss/bridgeApi/type/ApiCommonRequest.kt
@@ -4,6 +4,6 @@ import io.hss.bridgeApi.enums.MethodType
 
 data class ApiCommonRequest(
     val pathAndQuery: String,
-    val methodType: MethodType,
+    val method: MethodType,
     val body: Any,
 )
