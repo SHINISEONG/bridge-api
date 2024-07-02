f7f588b7ee17bf5d5fd7cf7ccfdbf3271a7ac147
shineseong <hss275989@gmail.com>
2024-07-02 14:32:07 +0900

    chore: version-up. 1.0.5 -> 1.0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "io.github.shiniseong.bridge-api"
-version = "1.0.5"
+version = "1.0.6"
 
 repositories {
     mavenCentral()
@@ -23,4 +23,43 @@ tasks.test {
 
 kotlin {
     jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenKotlin") {
+            from(components["kotlin"])
+
+            groupId = "io.github.shiniseong.bridge-api"
+            artifactId = "bridge-api"
+            version = "1.0.6"
+
+            pom {
+                name.set("Bridge API")
+                description.set("Bridge API is a simple library that helps facilitate communication between JavaScript and Android using REST API methods.")
+                url.set("https://github.com/shiniseong/bridge-api")
+
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("shiniseong")
+                        name.set("HEESEONG SHIN")
+                        email.set("hss275989@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/shiniseong/bridge-api.git")
+                    developerConnection.set("scm:git:ssh://github.com/shiniseong/bridge-api.git")
+                    url.set("https://github.com/shiniseong/bridge-api")
+                }
+            }
+        }
+    }
 }
